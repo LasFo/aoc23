532f8bc2e9156e512ec2a30bc28958c4557ff446
bob <lassefolger@gmail.com>
2023-12-04 19:16:09 +0100

    day04 2ez4me

--- 04/a.lua
+++ 04/a.lua
@@ -15,20 +15,10 @@ local function consumeLetters(s)
     return n, trimSpace(s:sub(de+1))
 end
 
-local function setDefault (t, d)
-    local mt = {__index = function () return d end}
-    setmetatable(t, mt)
-end
-
 io.input("input.txt")
 io.input("inputl.txt")
 local res = 0
-local cnt = 1
-local cards = {}
-setDefault(cards, 1)
 for line in io.lines() do
-    local numCards = cards[cnt]
-    res = res + numCards
     line = select(2, consumeLetters(line))
     line = select(2, consumeInt(line))
     line = line:sub(2)
@@ -48,12 +38,11 @@ for line in io.lines() do
         n, line = consumeInt(line)
         if not n then break end
         if win[n] then
-            c = c + 1
+            if c == 0 then c = 1
+            else c = c *2
+            end
         end
     end
-    for i = 1, c do
-        cards[cnt+i] = cards[cnt+i] + numCards
-    end
-    cnt = cnt + 1
+    res = res + c
 end
 print(res)
