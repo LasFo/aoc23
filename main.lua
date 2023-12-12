418176412e307541437b61e40722c42b4a4493ec
bob <lassefolger@gmail.com>
2023-12-12 20:59:45 +0100

    last commit this year. No longer able to solve the challenge

--- main.lua
+++ main.lua
@@ -1,60 +1,33 @@
-io.input("input.txt")
 io.input("inputl.txt")
+io.input("input.txt")
 
-local fun = require("fun")
+local function setDefault (t, d)
+  local mt = {__index = function () return d end}
+  setmetatable(t, mt)
+end
 
-local grid = {}
-local empty_rows = {}
-local galaxies = {}
-local i = 0
 for l in io.lines() do
-    i = i+1
     local row = {}
-    local empty = true
-    for j = 1, #l do
-        table.insert(row, l:byte(j))
-        if l:byte(j) == string.byte("#") then
-            table.insert(galaxies, {i, j})
-            empty = false
+    local i = 0
+    while true do
+        i = i + 1
+        if string.byte(" ") == l:byte(i) then break end
+        row[#row+1] = l:byte(i)
+    end
+    l = l:sub(i)
+    local numbers = {}
+    for n in l:gmatch("(%d+)") do
+        numbers[#numbers+1] = tonumber(n)
+    end
+    print(#row, #numbers)
+
+    local dp = {}
+    dp.setDefault(dp, 1)
+    for _, n in pairs(numbers) do
+        local newDp = {}
+        dp.setDefault(newDp, 0)
+        local sum = 0
+        for i = 1, #row do
         end
     end
-    if empty then table.insert(empty_rows, i) end
-    table.insert(grid, row)
 end
-
-local empty_cols = {}
-for x=1, #grid[1] do
-    local empty = true
-    for y=1, #grid do
-        if grid[y][x] == string.byte("#") then
-            empty = false
-        end
-    end
-    if empty then table.insert(empty_cols, x) end
-end
-
-local res = 0
-for x=1, #galaxies do
-    local a = galaxies[x]
-    for y=x+1, #galaxies do
-        local b = galaxies[y]
-        if a == b then goto endloop end
-        local s, l = a, b
-        if s[1] > l[1] then s, l = l, s end
-        local skipped_rows = 0
-        fun.each(function(c) if c > s[1] and c < l[1] then skipped_rows = skipped_rows +1 end end, empty_rows)
-        res = res + l[1] - s[1] + (skipped_rows*999999)
-
-        if s[2] > l[2] then s, l = l, s end
-        local skipped_cols = 0
-        fun.each(function(c) if c > s[2] and c < l[2] then skipped_cols = skipped_cols +1 end end, empty_cols)
-        res = res + l[2] - s[2] + (skipped_cols*999999)
-        ::endloop::
-    end
-end
-print(res)
-
---b = 0
---a = {1,2,3,4}
---fun.each(function(c) if c > 1 and c < 4 then b = b +1 end end, a)
---print(b)
